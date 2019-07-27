561425d5748c9785da45a973855196e9df5ccd3e
Federica Macchi <federica.macchi3@gmail.com>
2019-07-27 23:17:57 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+// Driver with name, age, and experience properties
+// PickupLocation with address and city properties
+// Make sure you run the tests to ensure your solutions work as expected.
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
