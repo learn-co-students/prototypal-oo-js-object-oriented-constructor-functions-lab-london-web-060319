a1f031212b903e4b3dbfbf75987c200028f6ccd7
github username <github email address>
2019-08-02 11:18:35 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(a, b, c){
+    this.year = a
+    this.color = b
+    this.model = c
+}
+
+function Driver(a, b, c){
+    this.name  = a
+    this.age = b
+    this.experience = c
+}
+
+function PickupLocation(a, b){
+    this.address = a
+    this.city = b
+}
